f336395bae0105e63bd7c4b88a0de37a9621752b
kup1o <kup1o@pm.me>
2023-06-19 11:25:35 +0300

    fix(config): change `from_slice` to `from_str`
    https://github.com/toml-rs/toml/blob/f4a5aa70eec0614a8f3dc0e3b1efbf930a033329/crates/toml/CHANGELOG.md?plain=1#LL62C1-L63C122
    https://github.com/toml-rs/toml/commit/9f98678bca9937e6f2bdf62a833d160bb1865cd6#diff-1a88e8355dd0785d71c72f80cb1981e532600d908e493996ae08da88015fbe9fR158

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,9 @@
 use log::error;
 use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
-use serde_json::from_slice;
 use std::env;
+use std::fs;
+use toml::de::from_str;
 
 const CONFIG_PATH_ENV: &str = "CONFIG_PATH";
 
@@ -31,8 +32,8 @@ pub struct Config {
 pub fn read_config() -> Config {
     env::var(&CONFIG_PATH_ENV)
         .map_err(|_| format!("{CONFIG_PATH_ENV} environment variable not set"))
-        .and_then(|config_path| std::fs::read(config_path).map_err(|e| e.to_string()))
-        .and_then(|bytes| from_slice(&bytes).map_err(|e| e.to_string()))
+        .and_then(|config_path| fs::read_to_string(config_path).map_err(|e| e.to_string()))
+        .and_then(|content| from_str(&content).map_err(|e| e.to_string()))
         .unwrap_or_else(|err| {
             error!("failed to read config: {err}");
             std::process::exit(1);
